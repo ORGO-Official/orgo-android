5e13bc194b6011e0b4dfde944f2c33054587f3db
ys05059 <ys05059@gmail.com>
2023-11-12 18:19:56 +0900

    feat : Logout, Withdraw UseCases

---
+++ core/domain/src/main/java/com/orgo/core/domain/usecase/auth/LogoutUseCase.kt
@@ -0,0 +1,18 @@
+package com.orgo.core.domain.usecase.auth
+
+import com.orgo.core.common.auth.KakaoAuthManager
+import com.orgo.core.common.auth.NaverAuthManager
+import com.orgo.core.data.repository.UserAuthRepository
+import com.orgo.core.model.data.SocialType
+import com.orgo.core.model.network.Resource
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
+import javax.inject.Inject
+
+class LogoutUseCase @Inject constructor(
+    private val userAuthRepository: UserAuthRepository,
+){
+    suspend operator fun invoke() : Flow<Resource<String>>{
+        return userAuthRepository.executeLogout()
+    }
+}
